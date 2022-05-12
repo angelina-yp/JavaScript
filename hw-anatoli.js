b51352a3b3a68e10f6e02e0de682c0057e530603
angelina-yp <104057573+angelina-yp@users.noreply.github.com>
2022-05-12 14:52:21 +0300

    Update hw-anatoli.js
    fix

--- hw-anatoli.js
+++ hw-anatoli.js
@@ -39,3 +39,38 @@ string += ":)";
 console.log(string);
 i++;
 }
+
+//2*. Преобразовать 2 задачу в функцию, принимающую на вход строку, которая и будет выводиться в консоль (как в условии смайлик), а также количество строк для вывода e.g. function printSmile(stroka, numberOfRows)
+
+const printSmile = function(stroka, numberOfRows) {
+    let result = ''
+    for (let i = 1; i <= numberOfRows; i++) {
+        result += stroka
+        console.log(result)
+    }
+}
+printSmile(':)', 5)
+
+
+//3**.  Написать функцию, которая принимает на вход слово. Задача функции посчитать и вывести в консоль, сколько в слове гласных, и сколько согласных букв.e.g. function getWordStructure(word) В консоли:Слово (word) состоит из  (число) гласных и (число) согласных букв
+//Проверки: 'case', 'Case', 'Check-list'
+
+function getWordStructure2(word) {
+    const vowels = 'aeiouy'.split('')
+    const consontants = 'bcdfghjklmnpqrstvwxz'.split('')
+    let numberOfVowels = 0
+    let numberOfConsontants = 0
+    for (char of word.toLowerCase()) {
+        if (vowels.includes(char)) numberOfVowels++
+        if (consontants.includes(char)) numberOfConsontants++
+    }
+    console.log(`Слово ${word} состоит из ${numberOfVowels} гласных и ${numberOfConsontants} согласных букв`)
+}
+
+
+//4**. Написать функцию, которая проверяет, является ли слово палиндромом e.g. function isPalindrom(word) Проверки: 'abba', 'Abba'
+
+function isPalindrom3(word) {
+    return word.toLowerCase() == word.toLowerCase().split('').reverse().join('')
+}
+console.log(`${isPalindrom3('Abba') ? 'It is a palindrome' : 'It is not a palindrome'}`)
