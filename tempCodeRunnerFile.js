7b68515c4a233bc6101a7d9788d46606e5040611
unknown <angelinyshechka@mail.ru>
2023-02-02 00:05:35 +0300

    add

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,15 +1,32 @@
-let arr = [1, 2, 3, 4]
-function f4(arr) {
-    let arr2 = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 === 0) {
-            arr2.push(arr[i])
-        }
-    }
-    if (arr2.length === 0) {
-        console.log("No even numbers")
-    } else {
-        console.log(arr2)
-    }
+function CheckList(web_ui) {
+    let arr = [['Ввести номер телефона состоящий только из цифр','вести номер делефона с граничными значениями','вести пробел'],['выбрать один чекбокс', 'выбрать несколько чекбоксов','оставить все чекбоксы не выбраными'],['нажать на кнопку SignIn',' нажать и удерживать кнопку SignIn','нажать кнопку SignIn двойным щелчком кнопки мыши']]
+if (web_ui === 'Phone number field'){
+for (let i in arr[0]){
+    console.log((+i+1)+ '.', arr[0][i])
 }
-f4(arr)
+}
+if (web_ui === 'CheckBox'){
+    for (let i in arr[1]){
+        console.log((+i+1)+ '.', arr[1][i])
+    }
+    }
+    if (web_ui === 'SignIn Button'){
+        for (let i in arr[2]){
+            console.log((+i+1)+ '.', arr[2][i])
+        }
+        }  
+}
+let web_ui = 'Phone number field'
+CheckList(web_ui)
+
+//7) Написать функцию которая на вход получает JSON а возвращяет XML
+function f7(JSON) {
+    let xml = '';
+    for (let key in JSON) {
+        xml += '<' + key + '>' + JSON[key] + '<' + key + '/>'
+        
+    }
+    console.log(xml)
+}
+let JSON = {name: 'a1', surname: 'a2', middlename: 'a3' }
+f7(JSON)
