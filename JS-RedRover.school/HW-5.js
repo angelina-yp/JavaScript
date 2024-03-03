08d857f8f917ac3f9c059903e612487c248a6654
Angelina <angelinyshechka@mail.ru>
2024-03-03 21:58:51 +0300

    hw

--- JS-RedRover.school/HW-5.js
+++ JS-RedRover.school/HW-5.js
@@ -1,101 +1,72 @@
+/*Homework 05. Course JavaScript.
+Задача.
+Выполняйте пошагово.
+1. Задайте объект room и его параметры:
+ключ height со значением 3
+ключ tv со значением samsung
+ключ big со значением true*/
+let room = {
+  height: 3,
+  tv: 'samsung',
+   big: true,
 
-//         01234567891011
-// console.log(str.length);
-// < str.length 
-// <= str.length - 1
-// for (let i=0; i < str.length; i++) {
-// console.log(str[i]); // текущий элемент строки
-// console.log(i);         // текущий индекс элемента
-// }
-// console.log(str.indexOf('W'));        // метод
-/*
-   for (let k=0; k < str.length; k++ ) { // цикл
-     if (str[k] === "W") {
-     console.log(k);
-     }
-   }
+}
+console.log(room);
 
-   for (let m=0; m < str.length; m++ ) { // цикл
-     if (m % 2 === 0) {                           // если остаток от деления индекса на 2 равен 0, то есть индекс чётный,
-     console.log('Чётный элемент: ' + str[m]);     // то выводим элемент строки, с указанным индексом
-     } else {
-     console.log('Нечётный элемент: ' + str[m]); 
-     }
-   }
-*/
-let arrayNumber = [1, 2, 3, 10, 20, 100];   // чётные элементы - 2, 10, 20, 100
-        //         0  1  2   3   4    5        чётные индексы - 0, 2, 4
-/*
-let sum = 0;
-let mult = 1;
-for (let x=0; x < arrayNumber.length; x++) {
-//    console.log(arrayNumber[x]);
-      sum += arrayNumber[x];        // сумма элементов массива
-      mult *= arrayNumber[x];       // произведение элементов массива
-}
-console.log(sum);
-console.log(mult);
+//2. Выведите в alert или console.log все параметры объекта room по очереди
+console.log(room.height);
+console.log(room.tv);
+console.log(room.big);
 
-let sum1 = 0;
-for (let y=0; y < arrayNumber.length; y++) {
- if (y % 2 === 0) {                             // если индекс элемента чётный, то cуммируем
- sum1 += arrayNumber[y];                      
- }   
-}
-console.log(sum1);
+//3. Выведите в alert или console.log тип данных параметра big
+console.log(typeof(room.big));
 
-let sumEvenElements = 0;
-for (let y=0; y < arrayNumber.length; y++) {
- if (arrayNumber[y] % 2 === 0) {                 // если текущий элемент массива чётный, то складываем
- sumEvenElements += arrayNumber[y];      
- }   
-}
-console.log(sumEvenElements);
-*/
-// НЕ ПУТАТЬ индексы с элементами массива!
+//4. Выведите в alert или console.log количество символов в строке параметра tv (длину строки)
 
-// Задача.  Просуммировать элементы массива, находящиеся по нечётному индексу.
+console.log(room.tv.length); 
+//5. Выведите в alert или console.log результат расчёта: длина строки параметра tv минус 1
+console.log(room.tv.length-1); 
 
-let arrayNumber1 = [1, 2, 3, 4, 5, 6, 7];    // Это элементы массива
-//                  0  1  2  3  4  5  6      // Это индексы массива
-let sumNumber = 0;                           // Сюда записываем результат суммы элементов массива
-/*
-for (let el=0; el < arrayNumber1.length; el++) {  // el - Это индекс элемента в массиве
-  if (el % 2 === 1) {                             // Если индекс нечётный, то значение элемента массива складываем 
-    sumNumber = sumNumber + arrayNumber1[el];     // 0 - чётное число
-    console.log(sumNumber); 
-  }
-}
+//6. Поменяйте samsung на все заглавные буквы и выведите в alert или консоль лог.
+console.log(room.tv.toUpperCase());
+//7. Замените samsung на LG в нашем объекте room
+room.tv = 'LG'
+console.log(room.tv);
+//8. 1. Добавьте в наш объект room ещё одну пару ключ-значение: ключ furniture, значение - массив из 3 строк: "table", "chair", "sofa"
+room.furniture = ["table", "chair", "sofa"];
+console.log('Обновленный объект room:', room);
 
-// console.log(sumNumber);                           //0 - 0 - 2 - 2 - 6 - 6 - 12 - 12
-// Чётные числа el % 2 == 0                     - остаток от деления равен 0
-// Нечётные числа el % 2 !== 0, el % 2 === 1    - остаток от деления НЕ равен 0 или равен 1
-for (let i in arrayNumber1) {                 // вывод индексов массива
-if (i % 2 === 1) {                             // Если индекс нечётный, то значение элемента массива складываем 
-    sumNumber = sumNumber + arrayNumber1[i];     // 0 - чётное число
-//    console.log(sumNumber); 
-    console.log('Индекс массива ' + i + ' Сумма элементов ' + sumNumber); 
-}
-}
+//8.2. выведите chair в console.log (путем обращения к массиву)
+console.log(room.furniture[1]); 
+//9. Удалите из нашего объекта room параметр big
+delete room.big;
+console.log(room); 
+//10.1. Задайте переменную paint со значением color
+let paint = 'color';
+//10.2. Создайте объект room и внутри него задайте ключ: в качестве имени ключа возьмите из переменной paint её значение color, в качестве значения присвойте blue
+let room1 = {};
 
-for (let el of arrayNumber1){                      // Элементы массива
-    sumNumber = sumNumber + el;                    // Сумма элементов массива, не обращаясь к индексам
-//    console.log(el);
-    console.log(sumNumber);
-}
-*/
-// let str = ["Hello", "World!"];
-//               0        1                          // индексы
-var str = 'Hello World! You are welcome'.split(" "); // разделить строку по пробелу и записать в массив, 
-                                                     // где каждый элемент - строка
-// console.log(str);
-for (let i=0; i < str.length; i++){               // i - это индекс слова
-//  console.log(str[i]);                          // текущий элемент строки - слово
-  for (let j=0; j < str[i].length; j++) {         // j - это индекс буквы слова
-  if (str[i][j] === 'W') {                        // i,j - это буква j для слова i
-    console.log(str[i]);
-//  console.log(str[i][j]);                         // выводим букву каждого слова
-  }
-  }
-}
-     
+room1[paint] = 'blue';
+
+ //10.3. Выведите в консоль лог ключ и его значение blue
+console.log(Object.keys(room1)[0] + ": " + Object.values(room1)[0]); 
+// Задача: Создать объект Testing, который обладает 7 принципами тестирования, и 4 типа тестирования обладают этими свойствами – 7-ью принципами тестирования из Силлабуса 4.0. Выведите в консоль текст, подтверждающий это. 
+// Создаем объект с 7 элементами
+  const Testing = {
+      element1: 'value1',
+      element2: 'value2',
+      element3: 'value3',
+      element4: 'value4',
+      element5: 'value5',
+      element6: 'value6',
+      element7: 'value7'
+  };
+  
+  // Создаем 4 объекта, которые обладают 7 элементами из объекта Testing
+  const object1 = { ...Testing };
+  const object2 = { ...Testing };
+  const object3 = { ...Testing };
+  const object4 = { ...Testing };
+  
+  console.log(object1);
+  
